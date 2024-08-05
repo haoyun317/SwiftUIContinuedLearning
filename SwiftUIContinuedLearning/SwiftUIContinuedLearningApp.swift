eb5069e9fb56ab8e2e20457e0b87ecbce5d1e1c5
HaoYun <haoyun0317@gmail.com>
2024-08-05 10:42:50 +0800

    Initial Commit

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by  謝皓昀 on 2024/8/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
