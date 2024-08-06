0914ff9ccc70a452542e4860b74c89b84112fb57
HaoYun <haoyun0317@gmail.com>
2024-08-06 11:01:02 +0800

    [Feature} 找到問題了 MagnificationGesture() 模仿IG上圖片的手勢縮放，放開後會回到原本大小

--- SwiftUIContinuedLearning/MagnificationGesture.swift
+++ SwiftUIContinuedLearning/MagnificationGesture.swift
@@ -12,23 +12,61 @@ struct MagnificationGesture: View {
     @State var lastAmount: CGFloat = 0
     
     var body: some View {
+        VStack(spacing: 10) {
+            HStack {
+                Circle().frame(width: 35, height: 35)
+                Text("The Rock")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding()
+//            Rectangle()
+            Image("theRock")
+                .resizable()
+                .frame(height: 300)
+                .scaleEffect(1 + currentAmount)
+                .gesture(
+                    SwiftUI.MagnificationGesture()
+                        .onChanged { value in
+                            currentAmount = value - 1
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                currentAmount = 0
+                            }
+                        }
+                )
+            HStack {
+                Image(systemName: "heart.fill")
+                Image(systemName: "text.bubble.fill")
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.headline)
+            Text("I was fortunate and I was lucky that l had a couple of people in my life who cared about me. I had good, loving parents.")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+        }
+        /*
         Text("Hello, World!")
             .font(.title)
             .padding(40)
             .background(Color.red.cornerRadius(10))
-            .scaleEffect(1 + currentAmount)
+            .scaleEffect(1 + currentAmount + lastAmount)
             // 'MagnificationGesture' will be deprecated in a future version of iOS: renamed to 'MagnifyGesture'
             .gesture(
-                //MagnificationGesture()
-                MagnifyGesture()
+            // 問題出在命名衝突上。您定義了一個名為 MagnificationGesture 的 View 結構，這與 SwiftUI 預設的 MagnificationGesture 手勢類型名稱相同。使用 SwiftUI. 前綴來明確指出您要使用的是 SwiftUI 模塊中的 MagnificationGesture。
+                SwiftUI.MagnificationGesture()
                     .onChanged { value in
-                        currentAmount = value.magnification
+                        currentAmount = value - 1
                     }
                     .onEnded { value in
                         lastAmount += currentAmount
                         currentAmount = 0
                     }
             )
+         */
+        
     }
 }
 
