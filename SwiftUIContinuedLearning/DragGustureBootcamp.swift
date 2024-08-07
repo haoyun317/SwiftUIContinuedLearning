9b650f8420f34ffe09c07cd838f036bd6160daa0
HaoYun <haoyun0317@gmail.com>
2024-08-07 10:48:25 +0800

    [Feature] Add scaleEffect,並建立一個自訂的縮放效果func

--- SwiftUIContinuedLearning/DragGustureBootcamp.swift
+++ SwiftUIContinuedLearning/DragGustureBootcamp.swift
@@ -21,6 +21,7 @@ struct DragGustureBootcamp: View {
             RoundedRectangle(cornerRadius: 20)
                 .frame(width: 300, height: 500)
                 .offset(offset)
+                .scaleEffect(getScaleAmount())
                 .gesture(
                     DragGesture()
                         .onChanged { value in
@@ -35,6 +36,13 @@ struct DragGustureBootcamp: View {
                         }
             )
         }
+    }
+    // 建立一個回傳值是CGFloat的func,因為.scaleEffect(CGFloat)
+    func getScaleAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percenttage = currentAmount / max
+        return 1.0 - min(percenttage, 0.5) * 0.5
     }
 }
 
