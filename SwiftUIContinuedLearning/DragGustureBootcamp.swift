faa56237989adf35a8167b5859bd343225231740
HaoYun <haoyun0317@gmail.com>
2024-08-07 10:25:01 +0800

    [Feature]  Add DragGesture

---
+++ SwiftUIContinuedLearning/DragGustureBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  DragGustureBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by  謝皓昀 on 2024/8/7.
+//
+
+import SwiftUI
+
+struct DragGustureBootcamp: View {
+    
+    @State var offset: CGSize = .zero
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 300, height: 500)
+            .offset(offset)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        withAnimation(.spring()) {
+                            offset = value.translation
+                        }
+                    }
+                    .onEnded { value in
+                        withAnimation(.spring()) {
+                            offset = .zero
+                        }
+                    }
+            )
+    }
+}
+
+#Preview {
+    DragGustureBootcamp()
+}
