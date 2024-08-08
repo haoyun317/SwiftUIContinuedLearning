9ce2c1e40ca70fff6a99259152458d76854b1dfd
HaoYun <haoyun0317@gmail.com>
2024-08-08 09:35:53 +0800

    [Feature] Add ScrollViewReader, 要注意 id, 這樣才知道該到哪個位置

---
+++ SwiftUIContinuedLearning/ScrollViewReaderBcamp.swift
@@ -0,0 +1,42 @@
+//
+//  ScrollViewReaderBcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by  謝皓昀 on 2024/8/8.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBcamp: View {
+    var body: some View {
+        ScrollView {
+//            ScrollViewReader(content: { proxy in
+//                /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+//            })
+            // 一樣的上下完全一下
+            ScrollViewReader { proxy in
+                Button("Click here to go to #49") {
+                   // proxy.scrollTo(id: Hashable, anchor: UnitPoint?)
+                    proxy.scrollTo(49, anchor: nil)
+                }
+                
+                ForEach(0..<50) { index in
+                    Text("This is item #\(index) ")
+                        .font(.headline)
+                        .frame(height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding()
+                    // let proxy know which "id" is which "index"
+                        .id(index)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewReaderBcamp()
+}
