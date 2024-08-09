b78603fa4844813c53b6c0b9ec0b9c0b6108d9b0
HaoYun <haoyun0317@gmail.com>
2024-08-09 10:37:40 +0800

    [Feature] 早期Music滑動專輯的效果

--- SwiftUIContinuedLearning/GeometryReaderBcamp.swift
+++ SwiftUIContinuedLearning/GeometryReaderBcamp.swift
@@ -9,17 +9,39 @@
 import SwiftUI
 
 struct GeometryReaderBcamp: View {
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double( 1 - (currentX / maxDistance))
+    }
+    
     var body: some View {
-        GeometryReader { geometry in
-            HStack(spacing: 0) {
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: geometry.size.width * 0.66)
-                Rectangle()
-                    .fill(Color.blue)
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            HStack {
+                ForEach(1..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 40),
+                                    axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
             }
-            .ignoresSafeArea()
-        }
+        })
+//        GeometryReader { geometry in
+//            HStack(spacing: 0) {
+//                Rectangle()
+//                    .fill(Color.red)
+//                    .frame(width: geometry.size.width * 0.66)
+//                Rectangle()
+//                    .fill(Color.blue)
+//            }
+//            .ignoresSafeArea()
+//        }
     }
 }
 
