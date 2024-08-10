e86fdcb40136316b11237b70937e22da89b16d85
HaoYun <haoyun0317@gmail.com>
2024-08-10 11:11:40 +0800

    [Feature] 三年前的教學了，加上動畫其實不會這樣也可以，多了解一種方法

--- SwiftUIContinuedLearning/MaskBcamp.swift
+++ SwiftUIContinuedLearning/MaskBcamp.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct MaskBcamp: View {
-    @State var rating: Int = 1
+    @State var rating: Int = 0
     
     var body: some View {
         ZStack {
-            HStack {
-                ForEach(1..<6) { index in
-                    Image(systemName: "star.fill")
-                        .font(.largeTitle)
-                        .foregroundStyle(rating >= index ? Color.yellow : Color.gray)
-                        .onTapGesture {
+            starsView
+                .overlay(overlayView.mask(starsView))
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundStyle(Color.yellow)
+                //width要求是CGFloat, rating / 5 等於一顆星, 再乘上geometry的寬度
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        // new! never seen before
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
                             rating = index
                         }
-                }
+                    }
             }
         }
     }
