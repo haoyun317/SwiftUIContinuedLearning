6078585752058efba9e09c71ad36b9c281266ec0
HaoYun <haoyun0317@gmail.com>
2024-08-11 11:04:42 +0800

    [Feature] 增加通知震動反饋形式(.success .warning .error)與反饋的強度(.light  .medium  .heavy)

---
+++ SwiftUIContinuedLearning/HapticsBcamp.swift
@@ -0,0 +1,30 @@
+//
+//  HapticsBcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by  謝皓昀 on 2024/8/11.
+//
+
+import SwiftUI
+
+class HapticManager {
+    static let instance = HapticManager()
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+    }
+}
+
+struct HapticsBcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticsBcamp()
+}
