8edac5c877e543630256c15600fd4de95fae41f1
HaoYun <haoyun0317@gmail.com>
2024-08-11 11:24:41 +0800

    [Done] 使用方式

--- SwiftUIContinuedLearning/HapticsBcamp.swift
+++ SwiftUIContinuedLearning/HapticsBcamp.swift
@@ -21,7 +21,17 @@ class HapticManager {
 
 struct HapticsBcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Button("success") { HapticManager.instance.notification(type: .success) }
+            Button("warning") { HapticManager.instance.notification(type: .warning) }
+            Button("error") { HapticManager.instance.notification(type: .error) }
+            Divider()
+            Button("heavy") { HapticManager.instance.impact(style: .heavy) }
+            Button("light") { HapticManager.instance.impact(style: .light) }
+            Button("medium") { HapticManager.instance.impact(style: .medium) }
+            Button("rigid") { HapticManager.instance.impact(style: .rigid) }
+            Button("soft") { HapticManager.instance.impact(style: .soft) }
+        }
     }
 }
 
