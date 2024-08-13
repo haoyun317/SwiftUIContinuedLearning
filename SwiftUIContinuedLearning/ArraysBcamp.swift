522cda8e3401bd4531deecb363a5120717d6ce1e
HaoYun <haoyun0317@gmail.com>
2024-08-13 09:08:20 +0800

    [View] 依照MVVM建立一個View架構

--- SwiftUIContinuedLearning/ArraysBcamp.swift
+++ SwiftUIContinuedLearning/ArraysBcamp.swift
@@ -14,7 +14,13 @@ struct UserModel: Identifiable {
     let isVerified: Bool
 }
 
+class ArrayModificationViewModel: ObservableObject {
+    
+}
+
 struct ArraysBcamp: View {
+    @StateObject var vm = ArrayModificationViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
