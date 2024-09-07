7fda0d8c691b6aec15a0f8dfd274e36fb42a4b94
HaoYun <haoyun0317@gmail.com>
2024-09-07 11:23:36 +0800

    [Feature] VisualEffect,想用geometryRader之前先試著用visualEffect,性能更好更方便

---
+++ SwiftUIContinuedLearning/VisualEffectBcamp.swift
@@ -0,0 +1,51 @@
+//
+//  VisualEffectBcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by  謝皓昀 on 2024/9/7.
+//
+
+import SwiftUI
+
+struct VisualEffectBcamp: View {
+    
+    //@State private var showSpacer: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 30) {
+                ForEach(0..<100) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.orange)
+                        .visualEffect { content, geometry in
+                            content
+                                .offset(x: geometry.frame(in: .global).minY * 0.09)
+                        }
+                }
+            }
+        }
+//        VStack {
+//            Text("(geometry.frame(in: .global).minY < 300 ? 1 : 0)")
+//                .padding()
+//                .background(Color.red)
+//                .visualEffect { content, geometry in
+//                    content
+//                        .grayscale(geometry.frame(in: .global).minY < 300 ? 1 : 0)
+//                        //.grayscale(geometry.size.width >= 200 ? 1 : 0)
+//            }
+//            if showSpacer {
+//                Spacer()
+//            }
+//        }
+//        .animation(.bouncy, value: showSpacer)
+//        .onTapGesture {
+//            showSpacer.toggle()
+//        }
+    }
+}
+
+#Preview {
+    VisualEffectBcamp()
+}
